5ddccd0d85d4522d1113b12f6bc2451f4a048594
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-07 02:03:17 +0200

    Add getComputerChoice function and getRandomNumber helper function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hey there!");
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getComputerChoice() {
+  let computerChoice;
+  const randomNumber = getRandomNumber(0, 2);
+  if (randomNumber === 0) {
+    computerChoice = `rock`;
+  } else if (randomNumber === 1) {
+    computerChoice = `paper`;
+  } else computerChoice = `scissors`;
+  return computerChoice;
+}
